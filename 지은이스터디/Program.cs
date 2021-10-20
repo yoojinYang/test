4fec0737fbb068297e73b7bb1f64ba3e7a0fb0dd
Yoojin Yang <yoojjin91@gmail.com>
2021-10-20 19:56:25 +0900

    초기 커밋.

---
+++ 지은이스터디/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace 지은이스터디
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder a = new StringBuilder();
+            //a.Append('a');
+            //a.Append('b');
+            //a.Append('c');
+
+            Console.WriteLine(a);
+            for (int j = 0; j < 5; j++)
+            {
+                for (int i = 0; i < 5; i++)
+                    a.Append('*');
+            }
+            Console.WriteLine(a);
+            //a.Clear();
+            //Console.WriteLine(a);
+
+            //a.Append('d');
+
+            //Console.WriteLine(a);
+        }
+    }
+}
+
+
+//a.Clear();
+
+//a.Clear();
